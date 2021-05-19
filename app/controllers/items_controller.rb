6db671d71f4433b44fff60425e5a24003b069fb3
xiangma0531 <shoma0531@outlook.com>
2021-05-19 17:49:44 +0900

    idがカラム名に含まれることによるエラー解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,9 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explain, :category_id, :status_id, :deli_change_id, :pref_id, :deli_days_id, :price).merge(user_id: current_user.id)
+  end
 end
