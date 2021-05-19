867ef9d74bf036b103b86177065b46b607e42d88
xiangma0531 <shoma0531@outlook.com>
2021-05-19 17:17:43 +0900

    バリデーション・アソシエーションをActiveHash仕様に修正

--- app/models/deli_days_id.rb
+++ app/models/deli_days_id.rb
@@ -3,4 +3,6 @@ class DeliDaysId < ActiveHash::Base
     {id: 1, name: '---'}, {id: 2, name: '1〜2日で発送'}, {id:3, name: '２〜3日で発送'},
     {id:4, name: '４〜7日で発送'}
   ]
+  include ActiveHash::Associations
+  has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :status_id
+  belongs_to :deli_change_id
+  belongs_to :pref_id
+  belongs_to :deli_days_id
+
   with_options presence: true do
     validates :name
     validates :explain
@@ -11,5 +18,13 @@ class Item < ApplicationRecord
     validates :deli_days_id
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid.'}
     validates :image
+  end
+
+  with_options numericality: {other_than: 1} do
+    validates :category_id
+    validates :status_id
+    validates :deli_change_id
+    validates :pref_id
+    validates :deli_days_id
   end
 end
