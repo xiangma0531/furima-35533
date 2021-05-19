f99432505ad0f80159a2fcd8a633cf754d7bc9c1
xiangma0531 <shoma0531@outlook.com>
2021-05-19 15:45:02 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
     validates :deli_change_id
     validates :pref_id
     validates :deli_days_id
-    validates :price
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid.'}
+    validates :image
   end
 end
