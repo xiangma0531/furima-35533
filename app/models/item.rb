1ec22b11a1f25eb7bb758e24e64d83549a3018be
xiangma0531 <shoma0531@outlook.com>
2021-05-19 15:32:34 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :category_id
+    validates :status_id
+    validates :deli_change_id
+    validates :pref_id
+    validates :deli_days_id
+    validates :price
+  end
 end
