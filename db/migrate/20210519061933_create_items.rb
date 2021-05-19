647bbc02eaa41ae5b68be0e9dbcaf5863f41ba61
xiangma0531 <shoma0531@outlook.com>
2021-05-19 15:24:11 +0900

    itemsテーブルを作成

--- db/migrate/20210519061933_create_items.rb
+++ db/migrate/20210519061933_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.text :explain, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :deli_change_id, null: false
+      t.integer :pref_id, null: false
+      t.integer :deli_days_id, null: false
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
