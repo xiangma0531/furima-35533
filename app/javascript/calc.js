9e4b875c6c9aaafd947bf89d5d49ad9e7f7629fd
xiangma0531 <shoma0531@outlook.com>
2021-05-20 10:49:27 +0900

    入力金額に応じて販売手数料・販売利益の計算結果を表示する実装

--- app/javascript/calc.js
+++ app/javascript/calc.js
@@ -1,3 +1,12 @@
-window.addEventListener('load', () => {
-  console.log('表示確認');
-});
+const calc = () => {
+  const itemPrice = document.getElementById("item-price")
+  itemPrice.addEventListener('input', () => {
+    const priceValue = itemPrice.value;
+    const addTaxPrice = document.getElementById("add-tax-price")
+    addTaxPrice.innerHTML = Math.floor(priceValue / 10)
+    const profit = document.getElementById("profit")
+    profit.innerHTML = Math.floor(priceValue * 0.9)
+  })
+}
+
+window.addEventListener('load', calc);
