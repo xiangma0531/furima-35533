94ecfb6f00de2bfa9882514bf30453d30c97c140
xiangma0531 <shoma0531@outlook.com>
2021-05-20 18:12:06 +0900

    出品者ではないログイン中ユーザーがURL直打ちした場合トップページに遷移させるよう実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -41,5 +42,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless current_user.id == item.user_id
+      redirect_to root_path
+    end
   end
 end
