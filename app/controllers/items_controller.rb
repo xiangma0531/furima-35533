7f837c1a88a72ec1b1eb9581f59626c0d83a1900
xiangma0531 <shoma0531@outlook.com>
2021-05-20 18:19:28 +0900

    Rubocopによるコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     item = Item.find(params[:id])
-    unless current_user.id == item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user.id == item.user_id
   end
 end
