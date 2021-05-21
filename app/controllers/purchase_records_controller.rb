39d68e2efe35d246850cc7fb9736a319b600e9f8
xiangma0531 <shoma0531@outlook.com>
2021-05-21 13:56:22 +0900

    購入画面への遷移を実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -4,5 +4,18 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
+    @purchase_record_delivery_address = PurchaseRecordDeliveryAddress.new(purchase_record_params)
+    if @purchase_record_delivery_address.valid?
+      @purchase_record_delivery_address.save
+      redirect_to root_path
+    else
+      render 'purchase_records#index'
+    end
+  end
+
+  private
+  def purchase_record_params
+    item = Item.find(params[:id])
+    params.require(:purchase_record_delivery_address).permit(:postal_code, :pref_id, :city, :address, :building, :phone_num).merge(user_id: current_user.id, item_id: item.id)
   end
 end
